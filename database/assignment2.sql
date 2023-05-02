32d7def4a32b8ca88d0763c869227b335005cf97
Isaac Cruz <76910938+cruzisaac51@users.noreply.github.com>
2023-05-02 15:32:56 -0600

    task2 assignment2

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -262,3 +262,18 @@ DELETE from account
 UPDATE inventory
     SET INV_DESCRIPTION = REGEXP_REPLACE(INV_DESCRIPTION,'small interiors', 'huge Interiors') 
     WHERE inv_model = 'Hummer';
+
+SELECT
+    inv_make,
+    inv_model,
+	classification_name
+    
+FROM
+    inventory
+INNER JOIN classification 
+    ON inventory.inv_id = classification.classification_id
+WHERE classification_name = 'Sport';
+
+UPDATE inventory
+set inv_image = REPLACE(inv_image, '/images/','/images/vehicles/'),
+	  inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
