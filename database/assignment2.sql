5fba043863e4cca0a15d216791ca660690e7833a
Isaac Cruz <76910938+cruzisaac51@users.noreply.github.com>
2023-05-02 16:41:16 -0600

    owner name fix

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,8 +1,14 @@
+--
+-- Database: "CSE 340 project"
+--Assignment 2, Task 2
+--
+
+
 CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', '<Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO democse340;
+    OWNER TO demo340;
 
 
 CREATE TABLE public.classification (
@@ -235,6 +241,11 @@ VALUES   (
     5
   );
 
+
+--Assignment 2, Task 1
+
+--insert Tony stark account
+
 INSERT INTO public.account (
     account_firstname,
     account_lastname,
@@ -249,20 +260,23 @@ VALUES (
 
 );
 
+--Update Tony to Admin
 UPDATE account
     set account_type = '<Admin' 
     where account_id = 1;
 
+
+--Delete Tony account
 DELETE from account 
     where account_id = 1;
 
-
-  
-
+--Update Hummer Interiors
 UPDATE inventory
     SET INV_DESCRIPTION = REGEXP_REPLACE(INV_DESCRIPTION,'small interiors', 'huge Interiors') 
     WHERE inv_model = 'Hummer';
 
+
+-- inner join 
 SELECT
     inv_make,
     inv_model,
@@ -274,6 +288,7 @@ INNER JOIN classification
     ON inventory.inv_id = classification.classification_id
 WHERE classification_name = 'Sport';
 
+--Update all records in the inventory table to add "/vehicles"
 UPDATE inventory
 set inv_image = REPLACE(inv_image, '/images/','/images/vehicles/'),
 	  inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
