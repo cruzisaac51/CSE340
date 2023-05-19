3b4b57f822ae9e367dc5db96b0b0ea41bc4ac4d8
Isaac Cruz <76910938+cruzisaac51@users.noreply.github.com>
2023-05-18 22:58:29 -0600

    week5

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const env = require("dotenv").config()
 const app = express()
 
 const expressLayouts = require("express-ejs-layouts")
+const baseController = require("./controllers/baseController")
 
 
 /* *************************
@@ -41,6 +42,7 @@ app.listen(port, () => {
 
 
 // Index route
-app.get('/', function(req, res) {
-  res.render('index',{title: 'Home page'})
-})
+app.get('/', baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", require("./routes/inventoryRoute"))
